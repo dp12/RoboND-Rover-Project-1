035bd572760f0cf95206f12288b3f31a9f968fff
dp12 <dp12@github.com>
2017-12-20 02:01:45 -0500

    Add astar implementation

---
+++ code/astar.py
@@ -0,0 +1,102 @@
+# -----------
+# User Instructions:
+#
+# Modify the the search function so that it becomes
+# an A* search algorithm as defined in the previous
+# lectures.
+#
+# Your function should return the expanded grid
+# which shows, for each element, the count when
+# it was expanded or -1 if the element was never expanded.
+#
+# If there is no path from init to goal,
+# the function should return the string 'fail'
+# ----------
+# DONE: use the heapq implementation shown here:
+# profile Python code with line_profiler @profile
+# https://stackoverflow.com/questions/3927628/how-can-i-profile-python-code-line-by-line
+# https://stackoverflow.com/questions/17328506/priorityqueue-is-very-slow
+from heapq import *
+
+grid = [[0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0]]
+heuristic = [[9, 8, 7, 6, 5, 4],
+             [8, 7, 6, 5, 4, 3],
+             [7, 6, 5, 4, 3, 2],
+             [6, 5, 4, 3, 2, 1],
+             [5, 4, 3, 2, 1, 0]]
+
+init = [0, 0]
+goal = [len(grid)-1, len(grid[0])-1]
+cost = 1
+
+delta = [[-1, 0 ], # go up
+         [ 0, -1], # go left
+         [ 1, 0 ], # go down
+         [ 0, 1 ]] # go right
+
+delta_name = ['^', '<', 'v', '>']
+
+def euclidean_heuristic(start, goal):
+    return np.linalg.norm(np.array(goal) - np.array(start))
+
+def search(grid,init,goal,cost,heuristic):
+    # ----------------------------------------
+    # modify the code below
+    # ----------------------------------------
+    closed = [[0 for col in range(len(grid[0]))] for row in range(len(grid))]
+    closed[init[0]][init[1]] = 1
+
+    expand = [[-1 for col in range(len(grid[0]))] for row in range(len(grid))]
+    action = [[-1 for col in range(len(grid[0]))] for row in range(len(grid))]
+
+    x = init[0]
+    y = init[1]
+    g = 0
+    # h = heuristic[x][y]
+    h = euclidean_heuristic([x, y], goal)
+    f = g + h
+
+    waypoints = []
+    open = []
+    heappush(open, (f, g, h, x, y))
+
+    found = False  # flag that is set when search is complete
+    resign = False # flag set if we can't find expand
+    count = 0
+
+    while not found and not resign:
+        if len(open) == 0:
+            resign = True
+            return "Fail"
+        else:
+            next = heappop(open)
+            x = next[3]
+            y = next[4]
+            g = next[1]
+            expand[x][y] = count
+            waypoints.append([x,y])
+            count += 1
+
+            if x == goal[0] and y == goal[1]:
+                found = True
+            else:
+                for i in range(len(delta)):
+                    x2 = x + delta[i][0]
+                    y2 = y + delta[i][1]
+                    if x2 >= 0 and x2 < len(grid) and y2 >=0 and y2 < len(grid[0]):
+                        if closed[x2][y2] == 0 and grid[x2][y2] == 0:
+                            g2 = g + cost
+                            # h2 = heuristic[x2][y2]
+                            h2 = euclidean_heuristic([x2, y2], goal)
+                            f2 = g2 + h2
+                            heappush(open, (f2, g2, h2, x2, y2))
+                            closed[x2][y2] = 1
+
+    return waypoints
+    # return expand
+
+print search(grid,init,goal,cost,heuristic)
